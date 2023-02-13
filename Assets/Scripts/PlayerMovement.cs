f1d905c86fbca211d96759e764f5d804921f1af1
ConcreteShield <sondinand@gmail.com>
2023-02-13 12:33:24 +0100

    Add camera movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   
+    //Cam movement
+    [SerializeField] private Transform _eyes;
+    [SerializeField] private float _sensitivity;
+    [Range(-90f, 0f)]
+    [SerializeField] private float _camLimitMin;
+    [Range(0f, 90f)]
+    [SerializeField] private float _camLimitMax;
+    private float _camAngle = 0.0f;
+
+    private void Update()
+    {
+        RotateEyes();
+        RotateBody();
+    }
+
+    private void RotateEyes()
+    {
+        float yMouse = Input.GetAxis("Mouse Y") * _sensitivity * Time.deltaTime;
+        _camAngle -= yMouse;
+        _camAngle = Mathf.Clamp(_camAngle, _camLimitMin, _camLimitMax);
+        _eyes.localRotation = Quaternion.Euler(_camAngle, 0, 0);
+    }
+
+    private void RotateBody()
+    {
+        float xMouse = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
+        transform.Rotate(Vector3.up * xMouse);
+    }
+}
